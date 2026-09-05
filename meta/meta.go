6ac2cdee94dd1cba5c3a8ed6811fade5d185904c
Aleksandr Smyshliaev <alex@gorka.lv>
2026-09-05 11:08:52 +0300

    chore: release version 0.1.0

--- meta/meta.go
+++ meta/meta.go
@@ -5,7 +5,7 @@ package meta
 var App = "sr"
 
 // Version is the semantic version string.
-var Version = "0.0.0"
+var Version = "0.1.0"
 
 // Date is the build timestamp.
 var Date = "1970-01-01T00:00:00Z"
